0afee53782fc2f39947ed66496b103be4ca05744
MitBhardiwala <meetbhardiwala@gmail.com>
2024-01-04 12:17:22 +0530

    updated Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import projImg1 from '../assets/img/Sample.png';
 import projImg2 from '../assets/img/Supermarket_System_Sample.png';
 import colorSharp2 from '../assets/img/color-sharp2.png'; // Import the image\
 import projImg3 from '../assets/img/project-img4.png';
+import projImg5 from '../assets/img/project-img5.png';
 
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -29,6 +30,12 @@ export const Projects = () => {
       description: 'Machine Learning Flask',
       imgUrl: projImg3,
       gitLink: 'https://github.com/MitBhardiwala/SI-GuidedProject-607041-1698684605',
+    },
+    {
+      title: 'Online Bike Showroom',
+      description: 'ReactJS NodeJS MongoDB Firebase',
+      imgUrl: projImg5,
+      gitLink: 'https://online-bike-showroom-eight.vercel.app/',
     },
   ];
 
